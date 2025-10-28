5654aa3e6b9977e2cb163eaa2f776e74577523ba
DanhDue ExOICTIF <danhdueexoictif@DanhDues-MacBook-Pro.local>
2025-10-29 01:15:03 +0700

    handle login/logout redirection.

--- django-nextjs-backend-api/src/waitlists/api.py
+++ django-nextjs-backend-api/src/waitlists/api.py
@@ -1,5 +1,6 @@
 from typing import List
 from ninja import Router
+from ninja_jwt.authentication import JWTAuth
 
 from django.shortcuts import get_object_or_404
 
@@ -9,14 +10,22 @@ from .schemas import WaitlistEntryListSchema
 router = Router()
 
 
-@router.get("", response=List[WaitlistEntryListSchema])
+@router.get(
+    "",
+    response=List[WaitlistEntryListSchema],
+    auth=JWTAuth(),
+)
 def list_waitlist_entries(request):
     # Logic to retrieve waitlist entries from the database
     qs = WaitlistEntry.objects.all()
     return qs
 
 
-@router.get("/{id}", response=WaitlistEntryListSchema)
+@router.get(
+    "/{id}",
+    response=WaitlistEntryListSchema,
+    auth=JWTAuth(),
+)
 def list_waitlist_entry(request, id: int):
     # Logic to retrieve a specific waitlist entry from the database
     obj = get_object_or_404(WaitlistEntry, id=id)
